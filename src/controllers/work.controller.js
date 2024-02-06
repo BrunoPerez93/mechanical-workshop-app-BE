1c4a0563034f979ba8353703b50d36b6350c7428
BrunoPerez <brunoperez24793@gmail.com>
2024-02-06 10:17:47 -0300

    fix problem with works display

--- src/controllers/work.controller.js
+++ src/controllers/work.controller.js
@@ -5,6 +5,7 @@ const { DateTime } = require("luxon");
 
 const createWork = async (workData) => {
   try {
+    console.log('workData', workData);
     return Work.create(workData);
   } catch (error) {
     console.log(error);
@@ -17,61 +18,77 @@ const createWork = async (workData) => {
 const getWorks = async (filters) => {
   try {
     const options = {
-      include: [{
-        association: Work.associations.mechanic,
-        where: {
-          ...(filters.mechanicName && {
-            userName: {
-              [Op.iLike]: `%${filters.mechanicName}%`
-            }
-          })
+      include: [
+        {
+          association: Work.associations.mechanic,
+          where: {
+            ...(filters.mechanicName && {
+              userName: {
+                [Op.iLike]: `%${filters.mechanicName}%`,
+              },
+            }),
+          },
+        },
+        {
+          association: Work.associations.carsModel,
+          include: [
+            {
+              association: CarsModel.associations.brand,
+            },
+          ],
+        },
+        {
+          association: Work.associations.client,
+          where: {
+            ...(filters.ci && {
+              ci: {
+                [Op.iLike]: `${filters.ci}%`,
+              },
+            }),
+            ...(filters.clientName && {
+              name: {
+                [Op.iLike]: `%${filters.clientName}%`,
+              },
+            }),
+          },
+        },
+      ],
+      where: Object.entries(filters).reduce((prev, [key, value]) => {
+        if (key === 'createdAt') {
+          prev[key] = {
+            [Op.and]: [
+              {
+                [Op.gte]: DateTime.fromISO(value, { zone: 'UTC-3' })
+                  .startOf('day')
+                  .toJSDate(),
+              },
+              {
+                [Op.lte]: DateTime.fromISO(value, { zone: 'UTC-3' })
+                  .endOf('day')
+                  .toJSDate(),
+              },
+            ],
+          };
+        } else {
+          prev[key] = {
+            [Op.iLike]: `%${value}%`,
+          };
         }
-      }, {
-        association: Work.associations.carsModel,
-        include: [{
-          association: CarsModel.associations.brand,
-        }],
-      }, {
-        association: Work.associations.client,
-        where: {
-          ...(filters.ci && {
-            ci: {
-              [Op.iLike]: `${filters.ci}%`
-            }
-          }),
-          ...(filters.clientName && {
-            name: {
-              [Op.iLike]: `%${filters.clientName}%`
-            }
-          })
-        }
-      },]
+        return prev;
+      }, {}),
+      order: [['createdAt', 'DESC']],
     };
-    options.where = Object.entries(filters).reduce((prev, [key, value]) => {
-      if (key === 'createdAt') {
-        prev[key] = {
-          [Op.and]: [
-            { [Op.gte]: DateTime.fromISO(value, { zone: 'UTC-3' }).startOf('day').toJSDate() },
-            { [Op.lte]: DateTime.fromISO(value, { zone: 'UTC-3' }).endOf('day').toJSDate() },
-          ]
-        }
-      } else {
-        prev[key] = {
-          [Op.iLike]: `%${value}%`
-        }
-      }
-      return prev;
-    }, {});
-    console.log('options', options)
+
+    console.log('options', options);
     options.attributes = { include: ['createdAt'] };
-    options.order = [['createdAt', 'DESC']];
-    options.attributes=  ['id', 'matricula', 'createdAt',];
+
     return Work.findAll(options);
   } catch (error) {
     console.log(error);
     throw new DatabaseError(Errors.databaseCreation);
   }
 };
+
 
 const getWork = async (id) => {
   try {
